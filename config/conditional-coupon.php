17e74507c7b3679189c4f6269e8f26574e64bde3
Mohammad Hossein <mehrabani.developer@gmail.com>
2025-10-03 00:40:01 +0330

    add config and migration

---
+++ config/conditional-coupon.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'conditions' => [
+        // Add custom check condition classes Implemented \MohammadMehrabani\ConditionalCoupon\CheckConditionAbstract
+        // for example:
+        // \App\CheckUsageLimitCondition::class => 'Usage limit',
+        // \App\CheckPaymentTypesCondition::class => 'Payment type restriction',
+        // \App\CheckPaymentMethodsCondition::class => 'Payment method restriction',
+    ],
+];
